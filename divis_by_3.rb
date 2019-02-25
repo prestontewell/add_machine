13101bd6fa2a04b2e6d7129f6f41fa242a4387a7
prestontewell <preston@tewellphoto.com>
2019-02-25 16:10:57 -0600

    fix divis_by_3 to reflect proper method practices

--- divis_by_3.rb
+++ divis_by_3.rb
@@ -1,12 +1,14 @@
-input_array = 0
 
-def divisible_by_three(input_array)
-  999.times do
-    input_array = input_array += 1
-    if input_array % 3 == 0
-      p input_array
+def divisible_by_three
+  i = 1
+  divisibles = []
+  1000.times do
+    i += 1
+    if i % 3 == 0
+      divisibles << i
     end    
   end
+  p divisibles
 end
 
-divisible_by_three(0)
+divisible_by_three
