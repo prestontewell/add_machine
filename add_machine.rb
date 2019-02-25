aa935255ffc366233acd36ca6baaab6b94129e77
prestontewell <preston@tewellphoto.com>
2019-02-25 10:31:52 -0600

    build adding machine 1 through 100

---
+++ add_machine.rb
@@ -0,0 +1,18 @@
+input_array= 0
+
+def adding_machine(input_array)
+  100.times do
+    input_array= input_array + 1
+    p input_array
+  end
+end
+
+p adding_machine(0)
+
+
+# a = 0
+
+# 10.times do
+#   a = a + 1
+#   p a
+# end
