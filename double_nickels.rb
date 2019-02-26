19455f93eee53fb0f0efd29ecfd6777bf3bd1a06
prestontewell <preston@tewellphoto.com>
2019-02-26 11:35:50 -0600

    create method that accepts array of numbers and returns the number of 55's found in the array

---
+++ double_nickels.rb
@@ -0,0 +1,15 @@
+# Write a method that accepts an array of numbers as a parameter, and returns the number of how many 55’s there are in the array. For example, if the input is [55, 4, 7, 55, 9, 1, 55, 2, 3, 55, 0], the output should be 4. 
+
+def double_nickels(input_array)
+  i = 0
+  counter = 0
+  input_array.length.times do     
+    if input_array[i] == 55
+      counter += 1
+    end
+    i += 1
+  end
+  p counter
+end
+
+double_nickels([45, 55, 7, 91, 55, 536, 12, 55, 9, 18, 99, 55])
